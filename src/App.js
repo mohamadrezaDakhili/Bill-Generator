8663e045b8f6d27a11a16cdd33a20bc77c9a50f4
Mohamadreza Dakhili <mohamadrezadakhili53@gmail.com>
2020-06-22 15:26:54 +0430

    fix layout bill generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import $ from "jquery";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="row justify-content-center p-0 m-0">
+      <div id="box-bill">
+        <div
+          id="box-top"
+          className="row justify-content-center align-items-center p-0 m-0"
         >
-          Learn React
-        </a>
-      </header>
+          رستوران مک‌دونالد شعبه کامرانیه
+        </div>
+        <div id="box-item"></div>
+        <div id="box-calc">
+          <div className="row p-0 m-0">
+            <div className="col-6 bg-danger">dg</div>
+            <div className="col-6">
+              <div id="test" className="row">
+                <div className="col-12 bg-dark">
+                  <div class="row justify-content-between">
+                    <span>820/900تومان</span>
+                    <span>جمع کل سفارشات</span>
+                  </div>
+                </div>
+                <div className="col-12 bg-warning">
+                  <div class="row justify-content-between">
+                    <span>20/500تومان</span>
+                    <span>حق سرویس و کارمزد</span>
+                  </div>
+                </div>
+                <div className="col-12 bg-primary">
+                  <div class="row justify-content-between">
+                    <span>34/000تومان</span>
+                    <span>تخفیف</span>
+                  </div>
+                </div>
+                <div className="col-12 bg-secondary">
+                  <div class="row justify-content-center">
+                    <span>894/450 تومان</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
